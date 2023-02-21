bd97b278c8978fc7e367875f50e43d06b9389ffa
Andrew Sosa <andrew@andrewsosa.com>
2023-02-20 19:35:25 -0500

    add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import "@picocss/pico/css/pico.min.css";
 import App from "./App.svelte";
 
+import * as Sentry from "@sentry/svelte";
+import { BrowserTracing } from "@sentry/tracing";
+
+// Initialize the Sentry SDK here
+Sentry.init({
+  dsn: "https://7fe6c13257b940b2934ba82326108664@o572617.ingest.sentry.io/5722220",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const app = new App({
   target: document.getElementById("app"),
 });
