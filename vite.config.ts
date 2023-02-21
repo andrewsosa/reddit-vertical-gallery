742e1e958fbb7281048e8bea6bf563df0510262f
Andrew Sosa <andrew@andrewsosa.com>
2023-02-20 19:57:36 -0500

    fix base path for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+
+const basePath = process.env.GH_PAGES ? "/reddit-vertical-gallery/" : "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: basePath,
   plugins: [svelte()],
-})
+});
